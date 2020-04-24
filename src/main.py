83ce36639a1a15c22f86846d5dc550e9850978b5
litao <light8lee@outlook.com>
2020-04-24 15:00:06 +0800

    change IO

--- src/main.py
+++ src/main.py
@@ -2,19 +2,55 @@
 
 from param_parser import parameter_parser
 from ego_splitter import EgoNetSplitter
-from utils import tab_printer, graph_reader, membership_saver
+import networkx as nx
+import numpy as np
+# from utils import tab_printer, graph_reader, membership_saver
 
-def main():
-    """
-    Parsing command line parameters, creating EgoNets.
-    Creating a partition of the persona graph. Saving the memberships.
-    """
-    args = parameter_parser()
-    tab_printer(args)
-    graph = graph_reader(args.edge_path)
-    splitter = EgoNetSplitter(args.resolution)
-    splitter.fit(graph)
-    membership_saver(args.output_path, splitter.overlapping_partitions)
+# def main():
+#     """
+#     Parsing command line parameters, creating EgoNets.
+#     Creating a partition of the persona graph. Saving the memberships.
+#     """
+#     args = parameter_parser()
+#     tab_printer(args)
+#     graph = graph_reader(args.edge_path)
+#     splitter = EgoNetSplitter(args.resolution)
+#     splitter.fit(graph)
+#     membership_saver(args.output_path, splitter.overlapping_partitions)
+
+from utils import load_dataset, load_feature, load_graph, output_res, evaluate
+
+def validation():
+    for name in ("../train/train_0.npz", "../train/train_1.npz", "../train/train_2.npz"):
+        graph = load_dataset(name)
+        splitter = EgoNetSplitter(1.)
+        G = nx.from_scipy_sparse_matrix(graph['A'])
+        print("size of G:", len(G.nodes()))
+        splitter.fit(G)
+        print("size of partitions:", len(splitter.overlapping_partitions))
+
+        output_res(splitter.overlapping_partitions, 'valid.txt')
+        pred = np.loadtxt('valid.txt', dtype=np.int32)
+        print("actual:", graph['Z'].shape[1])
+        print("Score:", evaluate(pred, graph['Z']))
+
+def predict():
+    graph_file = '../eval/graph.npz'
+    attr_file = '../eval/attr.npz'
+
+    train_graph = load_graph(graph_file)
+    # feature = load_feature(attr_file)
+
+    G = nx.from_scipy_sparse_matrix(train_graph)
+    print("size of G:", len(G.nodes()))
+    splitter = EgoNetSplitter(1.)
+    splitter.fit(G)
+    print("size of partitions:", len(splitter.overlapping_partitions))
+
+    output_res(splitter.overlapping_partitions, 'ego.txt')
+
 
 if __name__ == "__main__":
-    main()
+    # main()
+    validation()
+    predict()
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,149 @@
 """General data treatment utilities."""
 
 import json
-import torch
+# import torch
 import numpy as np
 import pandas as pd
 import networkx as nx
 from scipy import sparse
 from texttable import Texttable
+import scipy.sparse as sp
+
+def evaluate(result, label):
+    result_sum = sum(result)
+    result_index = np.argwhere(result_sum == 1)
+    result = np.delete(result, result_index, axis=1)
+    predict_label = np.zeros(label.shape)
+    for i in range(result.shape[1]):
+        tmp1 = np.dot(result[:, i].T, label)
+        tmp2 = (tmp1 == max(tmp1)).astype(float)
+        if np.sum(tmp2) > 1:
+            index = tmp2.argmax()
+            tmp2 = np.zeros(tmp1.shape)
+            tmp2[index] = 1
+        predict_label = predict_label + np.tile(tmp2, (result.shape[0], 1)) * np.tile(result[:, i],
+                                                                                      (label.shape[1], 1)).T
+    predict_label = (predict_label > 0).astype(float)
+    tmp3 = np.sum(label, 1)
+    p_label = np.tile((tmp3 > 0).astype(float), (label.shape[1], 1)).T * predict_label
+    R = np.sum(np.sum(predict_label * label)) / np.sum(np.sum(label))
+    P = np.sum(np.sum(predict_label * label)) / np.sum(np.sum(p_label))
+    F_score = 2 * R * P / (R + P)
+    if str(F_score) == 'nan':
+        F_score = 0
+    return F_score
+
+
+def load_graph(file_name):
+    graph = None
+    with np.load(file_name, allow_pickle=True) as loader:
+        loader = dict(loader)
+        A = sp.csr_matrix((loader['data'], loader['indices'],
+                           loader['indptr']), shape=loader['shape'])
+        # Remove self-loops
+        A = A.tolil()
+        A.setdiag(0)
+        graph = A.tocsr()
+    return graph
+
+
+def load_feature(file_name):
+    attr = None
+    with np.load(file_name, allow_pickle=True) as loader:
+        loader = dict(loader)
+        attr = sp.csr_matrix((loader['data'], loader['indices'],
+                           loader['indptr']), shape=loader['shape'])
+        attr = attr.tocsr()
+    return attr
+
+
+def load_dataset(file_name):
+    """Load a graph from a Numpy binary file.
+
+    Parameters
+    ----------
+    file_name : str
+        Name of the file to load.
+
+    Returns
+    -------
+    graph : dict
+        Dictionary that contains:
+            * 'A' : The adjacency matrix in sparse matrix format
+            * 'X' : The attribute matrix in sparse matrix format
+            * 'Z' : The community labels in sparse matrix format
+            * Further dictionaries mapping node, class and attribute IDs
+
+    """
+    if not file_name.endswith('.npz'):
+        file_name += '.npz'
+    with np.load(file_name, allow_pickle=True) as loader:
+        loader = dict(loader)
+        A = sp.csr_matrix((loader['adj_matrix.data'], loader['adj_matrix.indices'],
+                           loader['adj_matrix.indptr']), shape=loader['adj_matrix.shape'])
+
+        if 'attr_matrix.data' in loader.keys():
+            X = sp.csr_matrix((loader['attr_matrix.data'], loader['attr_matrix.indices'],
+                               loader['attr_matrix.indptr']), shape=loader['attr_matrix.shape'])
+        else:
+            X = None
+
+        Z = sp.csr_matrix((loader['labels.data'], loader['labels.indices'],
+                           loader['labels.indptr']), shape=loader['labels.shape'])
+
+        # Remove self-loops
+        A = A.tolil()
+        A.setdiag(0)
+        A = A.tocsr()
+
+        # Convert label matrix to numpy
+        if sp.issparse(Z):
+            Z = Z.toarray().astype(np.float32)
+
+        graph = {
+            'A': A,
+            'X': X,
+            'Z': Z
+        }
+
+        node_names = loader.get('node_names')
+        if node_names is not None:
+            node_names = node_names.tolist()
+            graph['node_names'] = node_names
+
+        attr_names = loader.get('attr_names')
+        if attr_names is not None:
+            attr_names = attr_names.tolist()
+            graph['attr_names'] = attr_names
+
+        class_names = loader.get('class_names')
+        if class_names is not None:
+            class_names = class_names.tolist()
+            graph['class_names'] = class_names
+
+        return graph
+
+
+def output_res(partition, outfile_name):
+    #排序
+    key_list = list(partition.keys())
+    key_list.sort()
+    max_p = 0
+    with open(outfile_name, 'w') as ofs:
+        for key in key_list:
+            if not partition[key]:
+                continue
+            curr_p = max(partition[key])
+            max_p = max(max_p, curr_p)
+        max_p += 1
+        print('max_p:', max_p)
+        for key in key_list:
+            vec = [0] * max_p
+            for p in partition[key]:
+                vec[p] = 1
+            line = ' '.join([str(x) for x in vec])
+            ofs.write(line + '\n')
+            
 
 def tab_printer(args):
     """
